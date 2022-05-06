e9eda9bdbcac406316aa3ba77e4dc2ad3d115aa7
DAVVEBOII <davhag@live.se>
2022-05-06 13:58:17 +0200

    JOCKE FOR LIFE

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,14 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Connection con = null;
+    public static void main(String[] args) throws SQLException {
+        Connection con;
         Statement stmt = null;
         ResultSet rs = null;
 
-        con = Connection.getConnection();
-        stmt = Connection.createStatement();
+        con = ConnectionMYDB.getConnection();
+        stmt=con.createStatement();
+
 
         //massa prints
         System.out.println("Welcome to LTU Library");
@@ -18,6 +19,8 @@ public class Main {
         System.out.println("Username");
         Scanner input1 = new Scanner(System.in);
         String usernameInput = input1.nextLine();
+        String findUser = "{ CALL spFindUser(?)}";
+        String userFromDB = null;
     }
 }
 
