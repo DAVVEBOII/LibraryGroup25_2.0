849db822b80876b422c3d26937079abdd2148353
diabeteskungen99 <lukasbergman@live.se>
2022-05-10 16:19:41 +0200

    Inloggning komplett utan gui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,10 @@ public class Main {
         Connection con;
         Statement stmt = null;
         ResultSet rs = null;
-
+        CallableStatement callableStatement = null;
 
         con = ConnectionMYDB.getConnection();
-        stmt=con.createStatement();
+        stmt = con.createStatement();
 
 
         //massa prints
@@ -19,28 +19,63 @@ public class Main {
         System.out.println("Please login");
         System.out.println("Username");
         Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
         String usernameInput = input1.nextLine();
-        String findUser = "{ CALL spFindUser(?)}";
+        String passwordInput = input2.nextLine();
+        String findUser = "{ CALL spFindUser(?) }";
+        String checkPassword = ("{ CALL spCheckPassword(?, ?) }");
         String userFromDB = null;
+        String pwdFromDB = null;
+
 
         try {
-        CallableStatement callableStatement = con.prepareCall(findUser);
+            callableStatement = con.prepareCall(findUser);
+            //callableStatement = con.prepareCall(checkPassword);
             callableStatement.setString(1, usernameInput);
-           rs = callableStatement.executeQuery();
+            //callableStatement.setString(2, passwordInput);
+            rs = callableStatement.executeQuery();
 
             while (rs.next()) {
                 userFromDB = rs.getString("username");
+
+                if (usernameInput.equals(userFromDB)) {
+                    System.out.println("User found");
+                } else {
+                    System.out.println("User not found");
+                    break;
+                } //end while
+
+
             }
-            if (usernameInput.equals(userFromDB)) {
-                System.out.println("User found");
-           } else {
-                System.out.println("User not found");
-            } //end while
-        } //end try
-         catch(SQLException sqlException){
-            sqlException.printStackTrace();
-          sqlException.getCause();
-        } //end catch
+
+
+            try {
+                callableStatement = con.prepareCall(checkPassword);
+                callableStatement.setString(1, usernameInput);
+                callableStatement.setString(2, passwordInput);
+                rs = callableStatement.executeQuery();
+
+                while (rs.next()) {
+                    pwdFromDB = rs.getString("pwd");
+                    if (passwordInput.equals(pwdFromDB)) {
+                        System.out.println("Password finns");
+                    } else {
+                        System.out.println("NEJ");
+                        break;
+                    }
+
+                }
+
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                ex.getCause();
+            } //end catch
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            ex.getCause();
+        }
+
     }
 }
 
